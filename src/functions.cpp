3af252eb909801a5f82ab5cb589dc67bf0ea9565
alexxelo <alyonkashmakova@gmail.com>
2021-01-07 22:00:38 +0700

    create methods for calculator

---
+++ src/functions.cpp
@@ -0,0 +1,25 @@
+#include "functions.h"
+
+int Menu::sum(int a, int b)
+{
+    return a + b;
+}
+
+int Menu::subtraction(int a, int b)
+{
+    return a - b;
+}
+
+int Menu::multiplication(int a, int b)
+{
+    return a * b;
+}
+
+int Menu::division(int a, int b)
+{
+    return a / b;
+}
+int Menu::powerOfNumber(int a, int b)
+{
+    return pow(a, b);
+}
---
+++ src/functions.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+class Menu {
+public:
+    
+    
+};
+#endif
